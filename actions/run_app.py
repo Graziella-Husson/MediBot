251d97481df4f569c9126608aed49be68abb7230
Graziella-Husson <graziella.husson1@gmail.com>
2018-08-08 10:23:40 +0200

    move run_app

--- actions/run_app.py
+++ actions/run_app.py
@@ -4,8 +4,8 @@ from rasa_core.interpreter import RasaNLUInterpreter
 from rasa_core.agent import Agent
 from rasa_core.interpreter import RegexInterpreter
 
-nlu_interpreter = RasaNLUInterpreter('./models/nlu/') #PATH TO NLU MODEL
-agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter) #PATH TO DIALOGUE MODEL
+nlu_interpreter = RasaNLUInterpreter('../models/nlu_model_en/') #PATH TO NLU MODEL
+agent = Agent.load('../models/dialogue_model', interpreter = nlu_interpreter) #PATH TO DIALOGUE MODEL
 
 input_channel = SlackInput(
    slack_token="",  # this is the `bot_user_o_auth_access_token`
